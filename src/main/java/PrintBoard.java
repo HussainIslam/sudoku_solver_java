a51c657f8253eda7cbc2d05586a00352e3b4cb2d
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-04-25 23:49:46 -0400

    refactored PrintBoard class to break into smaller methods

--- src/main/java/PrintBoard.java
+++ src/main/java/PrintBoard.java
@@ -6,65 +6,64 @@ public class PrintBoard {
     }
 
     public void printSudokuBoard(){
+        print(false);
+    }
 
+    public void printBoardStatus(){
+        print(true);
+    }
+
+    private void print(boolean printStatus) {
         for(int row = 0; row < 9; row++){
             if(row % 3 == 0){
-                for(int k = 0; k < 11 * 3; k++){
-                    System.out.print("-");
-                }
-                System.out.println("");
+                printHorizontalBorder();
             }
             for (int column = 0; column < 9; column++){
-                if(column % 3 == 0){
-                    System.out.print("| ");
-                }
-                int currValue = board.getACell(row, column).getFinalValue();
-                System.out.print(currValue == 0 ? " " : currValue);
+                printInsideVerticalBorder(column);
 
-                if(column == 8){
-                    System.out.print(" |");
-                } else{
-                    System.out.print("  ");
-                }
+                System.out.print(getValueForPrint(printStatus, row, column));
+
+                printLastVerticalBorder(column);
             }
             System.out.println("");
             if(row == 8){
-                for(int k = 0; k < 11 * 3; k++){
-                    System.out.print("-");
-                }
-                System.out.println("");
+                printHorizontalBorder();
             }
         }
     }
 
-    public void printBoardStatus(){
-        for(int row = 0; row < 9; row++){
-            if(row % 3 == 0){
-                for(int k = 0; k < 11 * 3; k++){
-                    System.out.print("-");
-                }
-                System.out.println("");
-            }
-            for (int column = 0; column < 9; column++){
-                if(column % 3 == 0){
-                    System.out.print("| ");
-                }
-                System.out.print(board.getACell(row, column).getCellStatus()? 1 : 0);
-
-                if(column == 8){
-                    System.out.print(" |");
-                } else{
-                    System.out.print("  ");
-                }
-            }
-            System.out.println("");
-            if(row == 8){
-                for(int k = 0; k < 11 * 3; k++){
-                    System.out.print("-");
-                }
-                System.out.println("");
-            }
+    private String getValueForPrint(boolean printStatus, int row, int column) {
+        String value;
+        if (printStatus){
+            value = board.getACell(row, column).getCellStatus() ? "1" : "0";
         }
+        else{
+            int cellValue = board.getACell(row, column).getFinalValue();
+            value = cellValue == 0 ? " " : String.valueOf(cellValue);
+        }
+        return value;
+    }
+
+
+    private void printInsideVerticalBorder(int column) {
+        if (column % 3 == 0) {
+            System.out.print("| ");
+        }
+    }
+
+    private void printLastVerticalBorder(int column) {
+        if (column == 8) {
+            System.out.print(" |");
+        } else {
+            System.out.print("  ");
+        }
+    }
+
+    private void printHorizontalBorder() {
+        for (int k = 0; k < 11 * 3; k++) {
+            System.out.print("-");
+        }
+        System.out.println("");
     }
 
 
